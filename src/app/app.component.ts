ae9af916d360534e3e02c71e59e8f69a70f44d5c
vtarun <vivektarun89@gmail.com>
2020-09-27 12:21:07 +0530

    clear all file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MyLibService } from 'my-lib';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +6,6 @@ import { MyLibService } from 'my-lib';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private myLibService: MyLibService){}
+  constructor(){}
   title = 'amazon-front-end';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,39 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }    from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing/app-routing.module';
-import { MyLibModule, MyLibService } from 'my-lib'
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AuthGuard } from './services/auth/auth.guard';
-import { NavigationModule } from './navigation/navigation.module';
-import { NavigationComponent } from 'src/app/navigation/navigation.component';
-import { AuthService } from 'src/app/services/auth/auth.service';
-import { CartComponent } from './cart/cart.component';
-import { ProfileComponent } from './profile/profile.component';
-import { LoginComponent } from './login/login.component';
-import { HttpService } from 'src/app/services/http/http.service';
-import { ProductModule } from 'src/app/product/product.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    CartComponent,
-    ProfileComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule,
-    NavigationModule,
-    ProductModule,
-    AppRoutingModule,
-       
+    FormsModule
   ],
-  providers: [AuthGuard, AuthService, HttpService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
